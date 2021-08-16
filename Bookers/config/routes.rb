b0969a23cf8cca87e21998fe2d7eb56fb4d077a8
Tonko27 <ec2-user@ip-172-31-80-51.ec2.internal>
2021-08-16 11:57:52 +0000

    [Finish]Assignment2-9

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+  root to: 'home#top'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  resources :books
+  
+ 
 end
